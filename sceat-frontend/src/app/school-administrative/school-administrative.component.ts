9751836fc5daa8139f18363487f9931efe3e8b13
Yordix <norbert2923@gmail.com>
2023-12-03 22:33:53 +0100

    Added Kitchen, School Administrative, Statistics pages. These are the 3 main pages

---
+++ sceat-frontend/src/app/school-administrative/school-administrative.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-school-administrative',
+  templateUrl: './school-administrative.component.html',
+  styleUrls: ['./school-administrative.component.css']
+})
+export class SchoolAdministrativeComponent {
+
+}
