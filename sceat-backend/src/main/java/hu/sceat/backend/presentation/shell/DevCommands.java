5be682e44e6723cd103e05b5a7a523a4c17fbdef
Trigary <trigary0@gmail.com>
2023-12-07 13:01:13 +0100

    improve dummy data generation

--- sceat-backend/src/main/java/hu/sceat/backend/presentation/shell/DevCommands.java
+++ sceat-backend/src/main/java/hu/sceat/backend/presentation/shell/DevCommands.java
@@ -10,8 +10,12 @@ import hu.sceat.backend.persistence.entity.Occasion;
 import org.springframework.shell.command.annotation.Command;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Command(command = "dev")
 public class DevCommands {
@@ -28,16 +32,51 @@ public class DevCommands {
 	@Command(command = "initDbWithDummyValues")
 	public String initDbWithDummyValues() {
 		if (orgService.list().size() > 0) return "Database is not empty, aborting";
+		ThreadLocalRandom random = ThreadLocalRandom.current();
 		
-		OrganizationId org = orgService.create("SampleSchool").orElseThrow();
+		OrganizationId org = orgService.create("Eötvös Középiskola").orElseThrow();
 		
 		UserId server = authService.registerServer("server-a@elte.hu", "password",
-				"ServerA", org.getId()).orElseThrow();
+				"Merő Melinda", org.getId()).orElseThrow();
 		authService.registerConsumer("consumer-a@elte.hu", "password",
-				"ConsumerA", org.getId());
+				"Éhes Elek", org.getId());
 		
-		menuService.create(server, org.getId(), "Vegetarian Lunch", LocalDate.now(), Occasion.LUNCH, 42,
-				List.of("Fish", "Chips"), Set.of(Allergen.FISH, Allergen.GLUTEN));
+		List<String> exampleMenuNames = List.of("Vegetáriánus menü", "Gluténmentes menü",
+				"Vegán menü", "Laktózmentes menü", "Halmentes menü", "Tojásmentes menü",
+				"Diabetikus menü", "Paleo menü", "Kóser menü", "Halal menü", "Szójamentes menü");
+		List<String> exampleFoods = new ArrayList<>(List.of("Csirke", "Rizs", "Krumpli", "Torta", "Puding",
+				"Pacal", "Káposzta", "Kolbász", "Sajt", "Kenyér", "Tészta", "Pizza", "Hamburger", "Hotdog",
+				"Sültkrumpli", "Sültcsirke", "Rántottcsirke", "Rántottsajt", "Rántottkolbász", "Rántottkarfiol",
+				"Rántottgomba", "Rántotttészta", "Rántottkrumpli", "Rántotttojás", "Rántottbrokkoli",
+				"Rántottkaralábé", "Rántottzöldség", "Tojásleves", "Gombaleves", "Húsleves", "Zöldségleves",
+				"Halászlé", "Bableves", "Krumplileves", "Palócleves", "Gulyásleves", "Káposztaleves", "Káposztásleves",
+				"Káposztásbab", "Káposztásbabfőzelék", "Lasagne", "Spagetti Bolognese", "Spagetti Carbonara"));
+		
+		LocalDate startDate = LocalDate.now().minusWeeks(1);
+		LocalDate endDate = LocalDate.now().plusWeeks(10);
+		for (LocalDate date = startDate; date.isBefore(endDate); date = date.plusDays(1)) {
+			if (random.nextDouble() < 0.3) continue; //skip day with 30% chance
+			
+			for (Occasion occasion : Occasion.values()) {
+				if (random.nextDouble() < 0.3) continue; //skip occasion with 30% chance
+				
+				Set<Allergen> allergens = new HashSet<>();
+				if (random.nextDouble() < 0.8) { //no allergens with 20% chance
+					for (Allergen allergen : Allergen.values()) {
+						if (random.nextDouble() < 2.0 / Allergen.values().length) allergens.add(allergen);
+					}
+				}
+				
+				String name = exampleMenuNames.get(random.nextInt(exampleMenuNames.size()));
+				int cost = 10 * random.nextInt(50, 300);
+				Collections.shuffle(exampleFoods);
+				List<String> foods = exampleFoods.stream()
+						.limit(random.nextInt(0, 5))
+						.toList();
+				
+				menuService.create(server, org.getId(), name, date, occasion, cost, foods, allergens);
+			}
+		}
 		
 		return "Database initialized with dummy data";
 	}
