24fbbb53b64aea7390f88aff618784d0cd527c19
Novák Lilla <novaklilla0621@gmail.com>
2023-12-17 11:55:45 +0100

    Added occasion field

--- sceat-frontend/src/app/school-administrative/item-manager/item-manager.component.ts
+++ sceat-frontend/src/app/school-administrative/item-manager/item-manager.component.ts
@@ -3,7 +3,10 @@ import { MenuItems } from 'src/app/models/menu-items';
 import { DataTransferService } from 'src/app/services/data-transfer.service';
 import {FormControl} from '@angular/forms';
 
-
+interface Occasions {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-item-manager',
@@ -16,8 +19,15 @@ export class ItemManagerComponent {
   public newData: MenuItems = new MenuItems();
   public input_foodname: string = '';
   public input_ingredients: string = '';
+  public input_occasion: string = '';
 
   public item: MenuItems = new MenuItems()
+
+  occasions: Occasions[] = [
+    {value: 'FORENOON_FOOD', viewValue: 'Tízórai'},
+    {value: 'LUNCH', viewValue: 'Ebéd'},
+    {value: 'AFTERNOON_FOOD', viewValue: 'Uzsonna'},
+  ];
 
   allergens = new FormControl('');
   allergensList: string[] = ['GLUTEN', 'PEANUTS', 'TREE_NUTS', 'CELERY', 'MUSTARD', 'EGGS', 'MILK', 'SESAME', 'FISH', 'CRUSTACEANS', 'MOLLUSCS', 'SOYA', 'SULPHITES', 'LUPIN', 'MEAT'];
@@ -33,13 +43,15 @@ export class ItemManagerComponent {
 
   onSubmit(){
     console.log("Submitted");
+    console.log(this.input_occasion);
     console.log(this.input_foodname);
     console.log(this.input_ingredients.split(/\s*,\s*/));
     console.log(this.allergens.value);
 
     this.newData = new MenuItems();
+    this.newData.occasion = this.input_occasion;
     this.newData.name = this.input_foodname;
-    for (var ingredient of this.input_ingredients.split(/\s*,\s*/)) {this.newData.foods.push(ingredient);}
+    for (var ingredient of this.input_ingredients.split(/\s*,\s*/)) {if (ingredient) {this.newData.foods.push(ingredient);}}
     for (var allergen of this.allergens.value!){this.newData.allergens.push(allergen);}
     
     this.allItems.push(this.newData);
