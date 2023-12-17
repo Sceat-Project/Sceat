b6022433b14fa050ea65d1684630e68735ed829e
Novák Lilla <novaklilla0621@gmail.com>
2023-12-17 10:33:58 +0100

    Modified routing

--- sceat-frontend/src/app/app-routing.module.ts
+++ sceat-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { KitchenComponent } from './kitchen/kitchen.component';
+import { SchoolAdministrativeComponent } from './school-administrative/school-administrative.component';
+import { StatisticsViewComponent } from './statistics-view/statistics-view.component';
 
 const routes: Routes = [
     {path: 'kitchen', component: KitchenComponent},
-    {path: 'admin', component: KitchenComponent},
-    {path: 'statistics', component: KitchenComponent}
+    {path: 'admin', component: SchoolAdministrativeComponent},
+    {path: 'statistics', component: StatisticsViewComponent}
 ];
 
 @NgModule({
